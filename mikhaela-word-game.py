f245d5c9d6fb75c28c1d59cafff7c2bd3fdf546e
Mikhaela <anderson.mikhaela@gmail.com>
2022-04-06 14:44:22 -0500

    run_game in process

--- mikhaela-word-game.py
+++ mikhaela-word-game.py
@@ -1,22 +1,35 @@
 #Game
 our_word = 'onomatopoeia'
 
-word = str and len > 1
+#word = str and len > 1
 
-letter = str and len == 1
+#letter = str and len == 1
 
 player_guess_num = 7
 
-show_word = ""
-for x in len(our_word):
-    show_word +='_'
-
-player_guesses =[]
+player_guesses = []
 
 length = len(our_word)
 
-player_guess = False
-while player_guess == False:
-    guess = str(input('Guess a letter or a word: '))
-    if guess != str:
-        print('That is not a word or a letter... try again.')
+show_word = '_' * length
+
+def run_guess():
+    player_guess = False
+    while player_guess == False and player_guess_num > 0:
+        guess = input('Guess a letter or a word: ')
+        print(type(guess))
+        if guess.isalpha():
+            if len(guess) > 1:
+                print(len(guess)) #This is the condition if it's a word
+            elif len(guess) == 1: #This is the condition if it's a letter
+                print
+        else:
+            print("That is not a lett or a word... try again.")
+            run_guess()
+
+        
+        #if type(guess) != str:
+            #print('That is not a word or a letter... try again.')
+
+run_guess()
+
