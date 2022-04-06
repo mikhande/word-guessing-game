9c082e6748c601b63bd6dc89e58ace6151c723cc
Mikhaela <anderson.mikhaela@gmail.com>
2022-04-06 15:37:24 -0500

    run_guess edits

--- mikhaela-word-game.py
+++ mikhaela-word-game.py
@@ -13,23 +13,27 @@ length = len(our_word)
 
 show_word = '_' * length
 
+def lose_turn():
+    player_guess_num = player_guess_num - 1
+
 def run_guess():
     player_guess = False
     while player_guess == False and player_guess_num > 0:
         guess = input('Guess a letter or a word: ')
-        print(type(guess))
         if guess.isalpha():
-            if len(guess) > 1:
-                print(len(guess)) #This is the condition if it's a word
+            if len(guess) > 1: #This is the condition if it's a word
+                if guess == our_word: #The person guesses word correctly
+                    print('Congratulations, you win!')
+                elif guess != our_word: #person guesses word incorrectly, loses turn, and guesses again.
+                    print("Sorry, that's not it. Try again.")
+                    lose_turn
+                    run_guess
+                print(len(guess)) 
             elif len(guess) == 1: #This is the condition if it's a letter
                 print
         else:
-            print("That is not a lett or a word... try again.")
+            print("That is not a letter or a word... try again.")
             run_guess()
-
-        
-        #if type(guess) != str:
-            #print('That is not a word or a letter... try again.')
 
 run_guess()
 
