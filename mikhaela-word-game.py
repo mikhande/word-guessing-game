ce21c2440badf26c990faf0dd280d80cf97d16fb
Mikhaela <anderson.mikhaela@gmail.com>
2022-04-06 11:02:01 -0500

    initial commit

---
+++ mikhaela-word-game.py
@@ -0,0 +1,15 @@
+our_word = 'onomatopoeia'
+
+player_guess_num = 7
+
+player_guess = str(input('Guess a letter or a word: '))
+
+word = str & len > 1
+
+letter = str & len == 1
+
+while player_guess == False:
+    guess ==
+
+if guess != word:
+    print('That is not a letter or a word, guess again')
